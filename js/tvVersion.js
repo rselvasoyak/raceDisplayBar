f3aeaba1f8bf11a84da43589214c1d800bd24110
Rana Soyak <rselvasoyak@gmail.com>
2023-08-09 15:04:51 -0400

    fetched the json data

--- js/tvVersion.js
+++ js/tvVersion.js
@@ -1,1 +1,13 @@
-console.log("hello");
+// Import mockJSON Data 
+const fetchData = () => {
+    fetch('./js/mockJSON.json')
+    .then((response) => response.json())
+    .then((json) => {
+        console.log(json)
+    });
+}
+
+fetchData();
+
+
+// Border Color Update 
